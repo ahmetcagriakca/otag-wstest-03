06e52041c7d59cc42cdab45b7e70964874724a95
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2026-04-14 13:38:14 +0300

    add asserts to modulo_sum

--- math_utils.py
+++ math_utils.py
@@ -1,1 +1,6 @@
 def modulo_sum(a, b, m): return (a+b)%m
+
+if __name__ == "__main__":
+    assert modulo_sum(3, 4, 5) == 2
+    assert modulo_sum(0, 0, 1) == 0
+    assert modulo_sum(10, 15, 7) == 4
