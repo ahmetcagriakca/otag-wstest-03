4b7efe7f61d8b51edbad61834405a07f89f8049b
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2026-04-14 13:36:13 +0300

    reduce math_utils to single modulo_sum line

--- math_utils.py
+++ math_utils.py
@@ -1,11 +1,1 @@
-def double(n): return n*2
-
-
-def modulo_sum(a, b, m):
-    return (a + b) % m
-
-
-assert double(3) == 6
-assert modulo_sum(2, 3, 4) == 1
-assert modulo_sum(10, 15, 7) == 4
-assert modulo_sum(0, 0, 5) == 0
+def modulo_sum(a, b, m): return (a+b)%m
